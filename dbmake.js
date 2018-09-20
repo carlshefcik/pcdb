0631e05732d4b063083978813dadeaac91a4bd81
carlshefcik <32783344+carlshefcik@users.noreply.github.com>
2018-09-19 18:21:06 -0700

    Initial commit

---
+++ dbmake.js
@@ -0,0 +1,21 @@
+/**
+ * Creates the db is not made and creates tables and relationships if not created already
+ */
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('EmployeeDB.db')
+
+db.serialize(function(){
+    db.run('CREATE TABLE IF NOT EXISTS Employees (PersonID INTEGER PRIMARY KEY AUTOINCREMENT, LastName varchar(255), FirstName varchar(255))')
+    
+    // insert command
+    //db.run('INSERT INTO Employees (LastName, FirstName) VALUES ("Shefcik", "Carl")')
+
+    // delete command
+    //db.run('DELETE FROM Employees WHERE PersonID=2 ')
+
+    db.all('SELECT * FROM Employees', (err, rows)=>{
+        console.log(rows)
+    })
+})
+
+db.close();
